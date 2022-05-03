c472829e771d615a24e9f1c63ee9f35b9ffe6ff9
Dalaran111 <dalaran111@gmail.com>
2022-05-04 01:53:24 +0300

    mobAdaptationStart

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
-                    arrows: false
+                    arrows: false,
+                    variableWidth: true,
+                    adaptiveHeight:true,
+                    dots: true
                 }
             }
         ]
@@ -34,7 +36,7 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-    //Modal
+    //Modal 
 
     $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultation').fadeIn();
